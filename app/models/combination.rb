f0e071091820d9601f99eebf83471637fc452d85
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-14 16:33:48 +0200

    validations

--- app/models/combination.rb
+++ app/models/combination.rb
@@ -2,4 +2,6 @@ class Combination < ApplicationRecord
   belongs_to :measure
   belongs_to :ingredient
   belongs_to :cocktail
+
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id, :measure_id]
 end
--- app/models/user_cocktail.rb
+++ app/models/user_cocktail.rb
@@ -1,4 +1,6 @@
 class UserCocktail < ApplicationRecord
   belongs_to :user
   belongs_to :cocktail
+
+  validates_uniqueness_of :user_id, :scope => :cocktail_id
 end
